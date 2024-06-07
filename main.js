835f0df1c3532619396169d7b729af354b208bf5
possido <134057973+possido@users.noreply.github.com>
2024-06-07 08:31:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ document.querySelectorAll('.botao').forEach(btn => {
         document.querySelectorAll('.aba-conteudo')[index].classList.add('ativo');
     });
 });
+
+const botoes = document.querySelectorAll(".botao");
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");{
+                botoes[i].classList.add("ativo");
+            }
+        }
+    }
+}
